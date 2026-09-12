aa6c6298f03a6006d64a8c95df258f169621c7ff
karanchauhan72 <kc946531@gmail.com>
2026-09-12 15:44:19 +0530

    Create config.py

---
+++ MeherMusic/config.py
@@ -0,0 +1,11 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_ID = int(os.getenv("API_ID", "0"))
+API_HASH = os.getenv("API_HASH", "")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "")
+MONGO_DB_URI = os.getenv("MONGO_DB_URI", "")
+OWNER_ID = int(os.getenv("OWNER_ID", "0"))
+STRING_SESSION = os.getenv("STRING_SESSION", "")
