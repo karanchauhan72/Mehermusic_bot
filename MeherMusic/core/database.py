ee677efe16034e64393f52ba30b87df2141166dc
karanchauhan72 <kc946531@gmail.com>
2026-09-24 01:02:53 +0530

    Delete MeherMusic directory

--- MeherMusic/core/database.py
+++
@@ -1,9 +0,0 @@
-class Database:
-    def __init__(self):
-        self.connected = False
-
-    async def connect(self):
-        self.connected = True
-
-    async def close(self):
-        self.connected = False
